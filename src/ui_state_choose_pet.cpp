51d05bbe7044be0916f2455bd4fa8f0fe2ee453f
Aaron Ayers <acp.ayers@gmail.com>
2026-03-26 01:12:19 -0500

    Enabled the eldritch pet in the public build.  Eldritch is a go!

--- src/ui_state_choose_pet.cpp
+++ src/ui_state_choose_pet.cpp
@@ -60,6 +60,7 @@ void uiChoosePetHandle(InputState& in)
   #if PUBLIC_BUILD
   static const PetType kChoices[] = {
     PET_DEVIL,
+    PET_ELDRITCH,
   };
 #else
   static const PetType kChoices[] = {
