ec9793d2719bcc819abc3f15d101e207430f15c7
Aaron Ayers <acp.ayers@gmail.com>
2026-04-17 15:23:39 -0500

    Updated version to match build, we are developing version 2.1.0

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#define RH_VERSION_MAJOR 1
+#define RH_VERSION_MAJOR 2
 #define RH_VERSION_MINOR 1
 #define RH_VERSION_PATCH 0
 
-#define RH_VERSION_LABEL "eldritch"
-#define RH_VERSION_STRING "2.0.0"
+#define RH_VERSION_LABEL "dev"
+#define RH_VERSION_STRING "2.1.0"
 
 #ifndef PUBLIC_BUILD
 #define PUBLIC_BUILD 0
