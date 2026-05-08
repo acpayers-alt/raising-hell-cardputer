f9d30bc7eae6086abf3677b47431eb33068b4a18
Aaron Ayers <acp.ayers@gmail.com>
2026-05-08 00:59:08 -0500

    avoid stale asset provision request when firmware marker write fails

--- src/boot_firmware_marker.cpp
+++ src/boot_firmware_marker.cpp
@@ -71,13 +71,13 @@ bool bootMarkFirmwareSeenAndRequestProvisionIfChanged()
     Serial.printf("[BOOT][FW] build changed old='%s' new='%s'\n",
                   lastSeenBuildId.length() ? lastSeenBuildId.c_str() : "(none)", currentBuildId.c_str());
 
-    requestAssetProvisionOnNextBoot();
-
     if (!bootFirmwareMarkerWrite(currentBuildId.c_str()))
     {
-      Serial.println("[BOOT][FW] failed to store current build id; not re-arming one-shot upgrade UI");
+      Serial.println("[BOOT][FW] failed to store current build id; skipping optional asset provision request");
       return false;
     }
+
+    requestAssetProvisionOnNextBoot();
   }
   else
   {
