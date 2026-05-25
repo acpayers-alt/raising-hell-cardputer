727fb425d4946321f593a28e0593978875b2eca9
Aaron Ayers <acp.ayers@gmail.com>
2026-05-25 15:47:50 -0500

    Add alien starship bridge background

--- src/graphics_pet_bg_paths.cpp
+++ src/graphics_pet_bg_paths.cpp
@@ -14,6 +14,7 @@ static const char *PATH_BG_ELDRITCH_ELDER = "/raising_hell/graphics/background/e
 
 static const char *PATH_BG_ALIEN_BABY = "/raising_hell/graphics/background/al/al_bb_bg.jpg";
 static const char *PATH_BG_ALIEN_TEEN = "/raising_hell/graphics/background/al/al_tn_bg.jpg";
+static const char *PATH_BG_ALIEN_ADULT = "/raising_hell/graphics/background/al/al_ad_bg.jpg";
 
 const char *bgPathForPet(PetType t)
 {
@@ -57,7 +58,10 @@ const char *bgPathForPetWithStage(PetType t, int evoStage)
 
   if (t == PET_ALIEN)
   {
-    if (evoStage >= 1)
+    if (evoStage >= 2)
+      return PATH_BG_ALIEN_ADULT;
+
+    if (evoStage == 1)
       return PATH_BG_ALIEN_TEEN;
 
     return PATH_BG_ALIEN_BABY;
